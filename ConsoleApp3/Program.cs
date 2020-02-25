9bb6ad8716f1ec6ffbab2b16ac294e367099ea7e
RayaMosholova <raq@abv.bg>
2020-02-25 17:17:14 +0200

    2moreExercisesAdded

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number1 = Console.ReadLine();
+            int a = int.Parse(number1);
+
+            string number2 = Console.ReadLine();
+            int b = int.Parse(number2);
+
+            Console.WriteLine((a*a) + (2*a*b) + (b*b));
+          
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string radius = Console.ReadLine();
+            int a = int.Parse(radius);
+      
+            Console.WriteLine(Math.PI * a * a);
+
+        }
+    }
+}
