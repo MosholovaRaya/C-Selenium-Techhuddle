30dea242d49ed316e95e79e55b67260fdfec1848
RayaMosholova <raq@abv.bg>
2020-02-25 17:46:41 +0200

    5thAdded

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int a = int.Parse(number);
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("True");
+}
+            else
+            {
+                Console.WriteLine("False");
+           }
+          
+        }
+    }
+}
