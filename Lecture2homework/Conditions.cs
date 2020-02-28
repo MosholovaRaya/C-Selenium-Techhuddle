9b213fa3d90c8548728014872d58eb6c8eb40a3f
RayaMosholova <raq@abv.bg>
2020-02-28 16:03:08 +0200

    latestExLecture2

---
+++ Lecture2homework/Conditions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture2homework
+{
+    class Conditions
+    {
+        public static void Ex1()
+        {
+
+        }
+            
+    }
+}
